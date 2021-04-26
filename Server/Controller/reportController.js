3517972a68e0bc11a9e7f460b55b07141f4a9d2c
gentcode1 <umutonigent98@gmail.com>
2021-04-26 09:51:15 +0200

    report clud

---
+++ Server/Controller/reportController.js
@@ -0,0 +1,78 @@
+import reportInfo from '../Model/ReportModel';
+import Response from '../Helpers/Response'
+
+class reportController{
+static createReport= async (req,res)=>{
+ let{
+    schoolId,
+    studentId,
+    report,
+     lesson,
+     marks,
+     test,
+     exams,
+     totalMarks,
+     markspercentage,
+     className,
+     academicYear,
+     gradeRetention
+ }=req.body
+ test= parseInt(req.body.test);
+ exams= parseInt(req.body.exams);
+ totalMarks= test+exams;
+ req.body.totalMarks; 
+ const reportData= await  reportInfo.create(req.body);
+
+ if(!reportData){
+ return Response.errorMessage(res, "no report info provided" , 404);
+ }
+ return Response.successMessage(res, "report created successfully", reportData,200);
+}
+static getOnereport= async (req, res)=>{
+    const reportId= req.params.id;
+ const reportData= await reportInfo.findById(reportId);
+ if(!reportData){
+  return Response.errorMessage(res, "can not find this report", 404);
+ }
+ return Response.successMessage(res, "get the report successfully", reportData, 200);
+}
+static getAllReport= async (req, res)=>{
+    const reportData= await reportInfo.find();
+    if (!reportData){
+   return  Response.errorMessage(res," can not get all reports",404)
+    }
+    return Response.successMessage(res, "get all reports successfully", reportData,200);
+}
+static updateReport=(req, res)=>{
+  let{
+    schoolId,
+    studentId,
+    report,
+     lesson,
+     marks,
+     test,
+     exam,
+     totalMarks,
+     marksPercentage,
+     className,
+     academicYear,
+     gradeRetention
+  }=req.body
+  const reportId= req.params.id;
+  const reportData= reportInfo.findByIdAndUpdate(reportId, {
+   lesson:lesson,
+   marks:marks,
+   test:est,
+   exams:exams,
+   totalMarks:totalMarks,
+    marksPercentage:marksPercentage,
+   gradeRetention:gradeRetention
+  });
+  if(!reportData){
+      return Response.errorMessage(res, "can not update report data" ,404);
+  }
+  const data= reportInfo.findById(reportId);
+  return Response.successMessage(res, "report updated successfully", data, 200);
+}
+}
+export default reportController;
